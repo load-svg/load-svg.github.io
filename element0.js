109d2fa5d1823a207223651a8d17f43af605d295
Danny Engelman <danny@engelman.nl>
2021-05-28 12:12:25 +0200

    Update element0.js

--- element0.js
+++ element0.js
@@ -1,1 +1,1 @@
-customElements.define("load-svg",class extends HTMLElement{async connectedCallback(t=this.attachShadow({mode:"open"})){t.innerHTML=await(await fetch(this.getAttribute("src"))).text();t.children[0].append(...this.children);this.hasAttribute("replace")&&this.replaceWith(t.children[0])}})
+customElements.define("load-svg",class extends HTMLElement{async connectedCallback(x=this.attachShadow({mode:"open"})){x.innerHTML=await(await fetch(this.getAttribute("src"))).text();x.firstChild.append(...this.children);this.hasAttribute("replace")&&this.replaceWith(x.firstChild)}})
